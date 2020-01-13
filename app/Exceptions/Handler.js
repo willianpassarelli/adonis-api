846248b9137667bc98eafb0ded28ab1cd725e7f7
willianpassarelli <willian.passarelli@hotmail.com>
2020-01-13 15:16:34 -0300

    config sentry

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Sentry = require('@sentry/node')
+const Config = use('Config')
+
 const Env = use('Env')
 const Youch = use('Youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
@@ -48,7 +51,9 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log(error)
+    Sentry.init({ dsn: Config.get('services.sentry.dsn') })
+
+    Sentry.captureException(error)
   }
 }
 
